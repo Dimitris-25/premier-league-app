e4991d2dc67adc9563185755158e3f612e17ee46
Dimitris <fenomeno979@gmail.com>
2025-09-12 23:38:25 +0300

    chore(ingest): remove countries.fetch.js and index.js

--- src/ingest/index.js
+++
@@ -1,7 +0,0 @@
-// Register all ingest routes here
-
-module.exports = function (app) {
-  require("./countries.fetch")(app);
-  // Later: require('./leagues.fetch')(app);
-  // Later: require('./venues.fetch')(app);
-};
