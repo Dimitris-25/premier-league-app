18b04d8be8fbdcf49051e0374a917e01a47077d4
Dimitris <fenomeno979@gmail.com>
2025-09-15 23:33:21 +0300

    feat(server): enable custom /api/v1 login routes; update deps/scripts

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 // Entry point to start the Feathers application
-
 const app = require("./src/app");
 const logger = require("./src/logger");
 
 const port = process.env.PORT || 3030;
 
-// Start server
+// Start Feathers app
 app.listen(port);
 
 // Log when server is listening
 app.on("listening", () => {
   logger.info(`Feathers app running at http://localhost:${port}`);
+  console.log(`Server: http://localhost:${port}`);
+  console.log(`Docs:   http://localhost:${port}/docs`);
 });
 
 // Log unexpected errors
